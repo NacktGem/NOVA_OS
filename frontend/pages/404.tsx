f3a0445cc8accf74cca9a8913f7d8be23b4808bf
NacktGem <nacktgem@gmail.com>
2025-08-05 13:25:50 -0500

    feat: add dynamic brand backgrounds

---
+++ frontend/pages/404.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <main className="flex items-center justify-center min-h-screen text-white">
+      <h1>404 – Page Not Found</h1>
+    </main>
+  );
+}
